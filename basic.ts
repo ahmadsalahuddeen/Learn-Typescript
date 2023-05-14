4e273e5dc569d68fb49620aefa4cb919ebcc9013
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-05-14 22:59:41 +0530

    array destrecturing

--- basic.ts
+++ basic.ts
@@ -1,6 +1,20 @@
-let name: string = 'salah'
+
+/**
+   * hold CTRL + press K and I : to get suggestion on Types of variable by place the cursor on the varibale name
+   *
+   */
+
+let Username: string = 'ahmad'      
 let isLoggedIn: boolean = true
 
-name += ' salah'
+Username += ' salah'
 
-console.log(name)
+
+
+let number: number = 19
+
+let regularExpression: RegExp = /foo/
+
+let [firstName , lastName]: string[] = Username.split(' ')
+
+console.log(firstName,' ', lastName)
