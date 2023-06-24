565f8aea38e65f6341fb5b9bce88f8df7f596484
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-06-24 13:47:32 +0530

    map class constructor

--- mini-app/src/index.ts
+++ mini-app/src/index.ts
@@ -1,16 +1,7 @@
 import { User } from "./User";
 import { Company } from "./Company";
+import {CustomMap} from './CustomMap'
+ 
+new CustomMap('map')
 
-
-const user = new User()
-const company = new Company()
-console.log(company)
-
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat:0,
-    lng:0
-  }
-});
 
