a2a5dbf0c7dc341626c26bba744b143f288ffd9d
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-06-24 12:56:44 +0530

    map api setup

--- mini-app/src/index.ts
+++ mini-app/src/index.ts
@@ -1,3 +1,16 @@
 import { User } from "./User";
+import { Company } from "./Company";
 
-console.log('hi')
+
+const user = new User()
+const company = new Company()
+console.log(company)
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat:0,
+    lng:0
+  }
+});
+
